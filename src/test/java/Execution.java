8a7a98fc7983b3edcfb1e73fcf80f0d442b641d6
Jaison0317 <jaison0317@gmail.com>
2024-11-09 00:10:50 -0400

    [Add] nuevos metodos de ejecucion

--- src/test/java/Execution.java
+++ src/test/java/Execution.java
@@ -1,12 +1,11 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-public class Login {
+public class Execution {
 
     private WebDriver driver;
     private static PageLogin Login;
